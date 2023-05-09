25a923e4a9c4074f1cff99770a94fdd03b1965cf
AgungWanda2209 <94591318+AgungWanda2209@users.noreply.github.com>
2023-05-09 20:31:46 +0800

    Create libsinit.js

---
+++ libsinit.js
@@ -0,0 +1,21 @@
+firebaseConfig = {
+
+    apiKey: "",
+
+    authDomain: "",
+
+    databaseURL: "https://indo-viral-new-default-rtdb.firebaseio.com",
+
+    projectId: "",
+
+    storageBucket: "",
+
+    messagingSenderId: "",
+
+    appId: "",
+
+};
+
+  firebase.initializeApp(firebaseConfig);
+
+  firebase.analytics();
