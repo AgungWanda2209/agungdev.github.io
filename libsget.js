b31631029f95a26f4f312399f3bce8924e68abe8
AgungWanda2209 <94591318+AgungWanda2209@users.noreply.github.com>
2023-05-09 20:55:42 +0800

    Update libsget.js

--- libsget.js
+++ libsget.js
@@ -8,6 +8,35 @@ window.onload=function(){
 
 
 function getdata(){
+    
+    firebase.database().ref('New/').once('value').then(function(snapshot){
+
+      
+
+    var posts_div=document.getElementById('New');
+
+      posts.innerHTML="";
+
+ 
+
+      var data=snapshot.val();
+
+      console.log(data);
+
+      for(let[key,value] of Object.entries(data)){
+
+        posts_div.innerHTML="<div class='eka'>"
+
+        +"<div class='cardagung'><p class='txtagung'>"+value.judul+"</p>"+
+
+        "<a href='"+value.url+"'><button class='btnagung'>Download</button></a>"+
+
+        "</div></div>"+posts_div.innerHTML;
+
+      } 
+
+});
+    
 
     firebase.database().ref('v2021/').once('value').then(function(snapshot){
 
@@ -68,5 +97,36 @@ function getdata(){
 
 
 });
+    
+    firebase.database().ref('v2023/').once('value').then(function(snapshot){
+
+      
+
+    var posts_div=document.getElementById('v2023');
+
+      posts.innerHTML="";
+
+ 
+
+      var data=snapshot.val();
+
+      console.log(data);
+
+      for(let[key,value] of Object.entries(data)){
+
+        posts_div.innerHTML="<div class='eka'>"
+
+        +"<div class='cardagung'><p class='txtagung'>"+value.judul+"</p>"+
+
+        "<a href='"+value.url+"'><button class='btnagung'>Download</button></a>"+
+
+        "</div></div>"+posts_div.innerHTML;
+
+      } 
+
+
+
+});
+
   
 }
